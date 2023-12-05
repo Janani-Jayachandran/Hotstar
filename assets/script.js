d7753e2acfe974a247aa996235190464128a202b
Janani-Jayachandran <jananijayachandran27@gmail.com>
2023-12-05 13:11:26 +0530

    added JS to play video cards

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,66 @@
-window.onload=function(){
+// window.onload=function(){
 
-    let m1=1324.4;
-    let fullwidth=document.getElementById('silder').offsetWidth;
-    let remainingwidth=fullwidth;
+//     let m1=1324.4;
+//     let fullwidth=document.getElementById('silder').offsetWidth;
+//     let remainingwidth=fullwidth;
 
-    document.getElementById("swipe-left").addEventListener("click", function(){
+//     document.getElementById("swipe-left").addEventListener("click", function(){
         
         
     
-    }) 
+//     }) 
     
-    document.getElementById("swipe-right").addEventListener("click",function(){
+//     document.getElementById("swipe-right").addEventListener("click",function(){
        
-        if(remainingwidth>m1)
-        {
-            document.getElementById('slider').style.marginLeft=-m1+"px";
-            remainingwidth=remainingwidth-1324.5;
-            m1=m1+1324.5;
-        }
-       else if(remainingwidth<m1){
-        document.getElementById('slider').style.marginLeft=-remainingwidth+"px";
-       }
-    })
-}
+//         if(remainingwidth>m1)
+//         {
+//             document.getElementById('slider').style.marginLeft=-m1+"px";
+//             remainingwidth=remainingwidth-1324.5;
+//             m1=m1+1324.5;
+//         }
+//        else if(remainingwidth<m1){
+//         document.getElementById('slider').style.marginLeft=-remainingwidth+"px";
+//        }
+//     })
+// }
 
+
+// video cards
+
+const videoCards=[...document.querySelectorAll('.video-card')];
+
+videoCards.forEach(item => {
+    item.addEventListener("mouseover", () => {
+        let video = item.children[1];
+        video.play();
+
+    });
+    item.addEventListener("mouseleave", () =>{
+        let video = item.children[1];
+        video.pause();
+    });
+});
+
+// slider
+
+
+// let cardContainers =  [...document.querySelectorAll(".card-container")];
+// let preBtns = [...document.querySelectorAll(".pre-btn")];
+// let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+// cardContainers.forEach((item, i) => {
+//     let containerDimensions = item.getBoundingClientRect();
+//     let containerWidth = containerDimensions.width;
+
+//     nxtBtns[i].addEventListener("click", () => {
+//         item.scrollLeft += containerWidth - 200;
+
+//     });
+
+//     preBtns[i].addEventListener("click", () => {
+//         item.scrollLeft -= containerWidth + 200;
+//     });
+
+// });
+
+ 
