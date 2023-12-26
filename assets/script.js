efc2fe14d03d28ed670f49d7749330a60493aa34
Janani-Jayachandran <jananijayachandran27@gmail.com>
2023-12-26 16:36:55 +0530

    added function to dropdown

--- assets/script.js
+++ assets/script.js
@@ -63,4 +63,38 @@ videoCards.forEach(item => {
 
 // });
 
- 
+// ------------------------  subscription page---------------------
+
+        
+const dropdowns = document.querySelectorAll('.dropdown');
+
+//  loop through all dropdown elements
+
+dropdowns.forEach(dropdown =>{
+
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const manu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () =>{
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        manu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option =>{
+        option.addEventListener('click', () =>{
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+
+            options.forEach(option =>{
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
